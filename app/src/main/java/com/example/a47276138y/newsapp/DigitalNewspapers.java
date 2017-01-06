9c58ec221fa7d0dbe0bee97680aba0af503652ca
Pau <fcpau4@gmail.com>
2017-01-06 14:22:32 +0100

    Creating the SettingsActivity and adding it to the Manifest as a child of MainActivity. OnOptionsItemSelected modified so that the SettingsActivity can be launched.

--- app/src/main/java/com/example/a47276138y/newsapp/DigitalNewspapers.java
+++ app/src/main/java/com/example/a47276138y/newsapp/DigitalNewspapers.java
@@ -8,7 +8,6 @@ public class DigitalNewspapers {
 
     private int id;
     private String name;
-    private String description;
     private String category;
     private String urlToLogos;
     private String country;
@@ -21,8 +20,6 @@ public class DigitalNewspapers {
     public void setId(int id) { this.id = id; }
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
     public String getCountry() { return country; }
     public void setCountry(String country) { this.country = country; }
     public String getUrlToLogos() { return urlToLogos;}
